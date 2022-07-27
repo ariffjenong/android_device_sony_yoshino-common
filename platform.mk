53c9eb72583675981aad3545e6c9a19fc160498e
ariffjenong <arifbuditantodablekk@gmail.com>
2022-07-28 00:24:49 +0700

    Revert "yoshino-common: Copy cgroups and task_profiles directly from system/core"
    This reverts commit 11dfdde.

--- platform.mk
+++ platform.mk
@@ -49,11 +49,6 @@ PRODUCT_COPY_FILES += \
 ### POWER
 TARGET_USE_CUSTOM_POWERHINT ?= false
 
-### POWER CGROUPS
-PRODUCT_COPY_FILES += \
-    system/core/libprocessgroup/profiles/cgroups_28.json:$(TARGET_COPY_OUT_VENDOR)/etc/cgroups.json \
-    system/core/libprocessgroup/profiles/task_profiles_28.json:$(TARGET_COPY_OUT_VENDOR)/etc/task_profiles.json
-
 ### RECOVERY
 include $(PLATFORM_PATH)/platform/*.mk
 
