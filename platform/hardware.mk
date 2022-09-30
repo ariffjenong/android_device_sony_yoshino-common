bbae3c1317eea471d6932f1dae0d2e144567cd27
johnmart19 <johnivan19999@gmail.com>
2022-09-30 14:57:34 +0700

    yoshino-common: Add compilation of legacy audio - We are recieving errors on Init about this, so let's satisfy environment: [    7.888683] init: Command 'restart vendor.audio-hal-4-0-msd' action=onrestart (<Service 'audioserver' onrestart>:2) took 0ms and failed: service vendor.audio-hal-4-0-msd not found [    7.888698] init: Command 'restart audio_proxy_service' action=onrestart (<Service 'audioserver' onrestart>:3) took 0ms and failed: service audio_proxy_service not found [    7.888722] init: Command 'restart vendor.audio-hal-2-0' action=onrestart (<Service 'audioserver' onrestart>:4) took 0ms and failed: service vendor.audio-hal-2-0 not found [    7.888729] init: Command 'restart audio-hal-2-0' action=onrestart (<Service 'audioserver' onrestart>:5) took 0ms and failed: service audio-hal-2-0 not found
    Change-Id: Ib668ff48a175cafe4b5e7a569832a06fe96d7e3f
    
    Signed-off-by: ariffjenong <arifbuditantodablekk@gmail.com>

--- platform/hardware.mk
+++ platform/hardware.mk
@@ -74,9 +74,10 @@ PRODUCT_PACKAGES += \
 ### SERVICES
 # AUDIO
 PRODUCT_PACKAGES += \
-    android.hardware.audio@4.0-impl:32 \
+    android.hardware.audio@4.0-impl \
+    android.hardware.audio@2.0-impl \
     android.hardware.audio@2.0-service \
-    android.hardware.audio.effect@4.0-impl:32 \
+    android.hardware.audio.effect@4.0-impl \
     android.hardware.soundtrigger@2.2-impl:32
 
 # BLUETOOTH
